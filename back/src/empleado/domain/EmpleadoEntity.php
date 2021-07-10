0a859bbedfdd6848cedb9cf74a3f08456376d8b7
Juan Pablo Marión <totomarion@gmail.com>
2021-07-09 23:05:58 -0300

    Agregar get y set IdUser

--- back/src/empleado/domain/EmpleadoEntity.php
+++ back/src/empleado/domain/EmpleadoEntity.php
@@ -97,4 +97,24 @@ final class EmpleadoEntity implements IEmpleadoEntity
             'email' => $this->getEmail()
         ];
     }
+
+    /**
+     * Get the value of userId
+     */ 
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set the value of userId
+     *
+     * @return  self
+     */ 
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
 }
